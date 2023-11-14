fa2243a88d2536514f522687e1f4a9abb246d8a0
HazemBZ <hazembenbz@gmail.com>
2023-11-14 10:25:43 +0100

    feat: Add "next" button

--- src/main.js
+++ src/main.js
@@ -1,12 +1,19 @@
 import World from './World'
 
-function main() {
+async function main() {
   const container = document.querySelector('#scene-container')
 
   const world = new World(container)
 
-  world.start()
   // world.render()
+
+  await world.init()
+
+  document.querySelector('#next').addEventListener('click', () => world.focusNext())
+
+  world.start()
 }
 
-main()
+main().catch(err => {
+  console.error(err)
+})
